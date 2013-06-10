ac4f345dddc9cd86d8d6fe6b5b64df420cc5efca
Nebojsa Petrovic <nebspetrovic@gmail.com>
2013-06-09 20:43:34 -0700

    Add base demo project
    Added a base demo xcode project.  Nothing concrete implemented yet.

---
+++ NPFoundationDemo/NPFoundationDemo/NPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NPAppDelegate.h
+//  NPFoundationDemo
+//
+//  Created by Nebs on 6/9/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NPFoundationDemo/NPFoundationDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NPFoundationDemo
+//
+//  Created by Nebs on 6/9/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NPAppDelegate class]));
+    }
+}
