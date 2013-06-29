6b3cc28093e4cedffbf321781b5e98c4504d1d8e
Nebojsa Petrovic <nebspetrovic@gmail.com>
2013-06-28 19:26:08 -0700

    Add peek, push and pop to NPStack
    Added peek, push and pop functions to NPStack including specs.

--- NPFoundationDemo/NPFoundationDemo/NPAppDelegate.m
+++ NPFoundationDemo/NPFoundationDemo/NPAppDelegate.m
@@ -7,17 +7,10 @@
 //
 
 #import "NPAppDelegate.h"
-#import "NPLinkedList.h"
-#import "NPStack.h"
 
 @implementation NPAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
-    NPStack *stack = [NPStack stack];
-    [stack pushObject:@"foo"];
-    NSLog(@"Top object: %@", [stack peekObject]);
-
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
